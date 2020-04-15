46cf844b9dc2da4e10f5b8223dc694e1d0928b39
FMGordillo <facundomgordillo@gmail.com>
2020-04-15 11:50:10 -0300

    Updates

---
+++ precache-manifest.5d751b2ae9d93b58414fc56f3dfa5b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60383f2318d0d9a48e6b4b38f061a3b0",
+    "url": "/FMGordillo/jpmorgan-selection-react/index.html"
+  },
+  {
+    "revision": "50994fdc44c566563991",
+    "url": "/FMGordillo/jpmorgan-selection-react/static/css/2.ce91367d.chunk.css"
+  },
+  {
+    "revision": "8ed0eaab0b678f4c0cb5",
+    "url": "/FMGordillo/jpmorgan-selection-react/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "50994fdc44c566563991",
+    "url": "/FMGordillo/jpmorgan-selection-react/static/js/2.b50e86bc.chunk.js"
+  },
+  {
+    "revision": "031722a6da4bf6bddfd0bdb7793ff9da",
+    "url": "/FMGordillo/jpmorgan-selection-react/static/js/2.b50e86bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed0eaab0b678f4c0cb5",
+    "url": "/FMGordillo/jpmorgan-selection-react/static/js/main.598131a3.chunk.js"
+  },
+  {
+    "revision": "c8225e3937e6ba77875a",
+    "url": "/FMGordillo/jpmorgan-selection-react/static/js/runtime-main.c057c2a3.js"
+  }
+]);
